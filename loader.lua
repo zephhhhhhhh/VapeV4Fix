6ff0553ddad405d80fe2b445b195fa038915d27e
zeph <skidddinf@gmail.com>
2025-03-15 17:06:35 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -11,7 +11,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/VapeV4Fix/VapeV4Fix/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/zephhhhhhhh/VapeV4Fix/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
